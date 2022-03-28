aa86e4aac71f5fda05f964fbc24924f0ae62b814
Sammy Breen <s.breen@tum.de>
2022-03-28 21:43:54 +0200

    Added second transport to results

--- src/plots.js
+++ src/plots.js
@@ -1,12 +1,13 @@
-const ctx = document.getElementById("myChart");
 d3.csv("./data/grandlgruber.csv").then(makeChart);
+d3.csv("./data/schenkerG.csv").then(makeSchenkerChart);
 
 function makeChart(rawData) {
+  let canvas = document.getElementById("plot-grandlgruber");
   let measurement = rawData.map((datapoint) => Number(datapoint.measurement));
   let simulation = rawData.map((datapoint) => Number(datapoint.simulation));
   let weather = rawData.map((datapoint) => Number(datapoint.weather));
   let labels = rawData.map((datapoint) => datapoint.date.split(" ")[1]);
-  const myChart = new Chart(ctx, {
+  const myChart = new Chart(canvas, {
     type: "line",
     data: {
       labels: labels,
@@ -28,6 +29,38 @@ function makeChart(rawData) {
           data: weather,
           borderColor: "#00CAA4",
           backgroundColor: "#00CAA4",
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+    },
+  });
+}
+
+function makeSchenkerChart(rawData) {
+  let canvas = document.getElementById("plot-schenkerG");
+  let simulation = rawData.map((datapoint) => Number(datapoint.simulation));
+  let weather = rawData.map((datapoint) => Number(datapoint.weather));
+  let labels = rawData.map((datapoint) => datapoint.date.split(" ")[1]);
+  const myChart = new Chart(canvas, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Simulation",
+          data: simulation,
+          borderColor: "#5789d7",
+          backgroundColor: "#5789d7",
+          pointRadius: 0,
+        },
+        {
+          label: "Ambient temperature",
+          data: weather,
+          borderColor: "#00CAA4",
+          backgroundColor: "#00CAA4",
+          pointRadius: 0,
         },
       ],
     },
